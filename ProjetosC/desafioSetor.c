17cd0b545778b555db0a54b4075c75b278017914
Pedro Henrique S.C. Lima <117484349+PedroSCL@users.noreply.github.com>
2023-09-20 21:38:12 -0300

    funcional agora

--- ProjetosC/desafioSetor.c
+++ ProjetosC/desafioSetor.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <locale.h>
 
-char nome[50], setor;
+char nome[50], setor[4];
 
-main(void){
-	setlocale(LC_ALL, "");
-	printf("Digite seu nome completo: ");
-	scanf("%s", &nome);
-	printf("Digite o seu setor: ");
-	fflush(stdin);
-	scanf("%s", &setor);
+int main(void) {
+    setlocale(LC_ALL, "");
+    printf("Digite seu nome completo: ");
+    scanf("%s", nome);
+    printf("Digite o seu setor: ");
+    fflush(stdin);
+    scanf("%s", setor);
 
-	if((setor == 'A') || (setor[0] == 'a') || (setor == "111") || (setor == 'tec') || (setor == 'TEC')){
-		printf("Selecionado setor: T�cnico \n \n");
-	} else if((setor == 'B') ||(setor == 'b') || (setor == '112') || (setor == 'an') || (setor == 'AN')){
-		printf("Selecionado setor: Analista \n \n");
-	}else if((setor == 'C') ||(setor == 'C') || (setor == '122') || (setor == 'ger') || (setor == 'GER')){
-		printf("Selecionado setor: Gerente \n \n");
-	}else if((setor == 'D') ||(setor == 'd') || (setor == '233') || (setor == 'dir') || (setor == 'DIR')){
-		printf("Selecionado setor: Diretor \n \n");
-}
+    if ((setor[0] == 'A' || setor[0] == 'a') || strcmp(setor, "111") == 0 || (setor[0] == 't' && setor[1] == 'e' && setor[2] == 'c') || (setor[0] == 'T' && setor[1] == 'E' && setor[2] == 'C')) {
+        printf("Selecionado setor: Técnico\n\n");
+    } else if ((setor[0] == 'B' || setor[0] == 'b') || strcmp(setor, "112") == 0 || (setor[0] == 'a' && setor[1] == 'n') || (setor[0] == 'A' && setor[1] == 'N')) {
+        printf("Selecionado setor: Analista\n\n");
+    } else if ((setor[0] == 'C' || setor[0] == 'c') || strcmp(setor, "122") == 0 || (setor[0] == 'g' && setor[1] == 'e' && setor[2] == 'r') || (setor[0] == 'G' && setor[1] == 'E' && setor[2] == 'R')) {
+        printf("Selecionado setor: Gerente\n\n");
+    } else if ((setor[0] == 'D' || setor[0] == 'd') || strcmp(setor, "233") == 0 || (setor[0] == 'd' && setor[1] == 'i' && setor[2] == 'r') || (setor[0] == 'D' && setor[1] == 'I' && setor[2] == 'R')) {
+        printf("Selecionado setor: Diretor\n\n");
+    } else {
+        printf("Setor não reconhecido.\n\n");
+    }
+
+    return 0;
 }
